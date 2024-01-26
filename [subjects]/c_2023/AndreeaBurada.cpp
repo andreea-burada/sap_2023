1c507c9b286215f6825223f35d9ab86a228aa1fb
Andreea B <burada.andreea@outlook.com>
2024-01-26 16:05:14 +0200

    Solved c_2023 subject

--- [subjects]/c_2023/AndreeaBurada.cpp
+++ [subjects]/c_2023/AndreeaBurada.cpp
@@ -4,39 +4,45 @@
 #include <string.h>
 #include <openssl/sha.h>
 #include <openssl/aes.h>
+#include <openssl/applink.c>
+#include <openssl/pem.h>
+#include <openssl/rsa.h>
+
+#pragma warning(disable : 4996)
+#define _CRT_SECURE_NO_WARNINGS
 
 #define MESSAGE_CHUNK 256
 #define _BUFFER 128
 
 unsigned char* hashPassword(unsigned char* _password) {
-	// create local copy of password
-	int _p_length = strlen((char*)_password);
+	int passwordLength = strlen((char*)_password);
 
 	SHA256_CTX ctx;
 	SHA256_Init(&ctx);
-	unsigned char _final_digest[SHA256_DIGEST_LENGTH];
+	unsigned char finalDigest[SHA256_DIGEST_LENGTH];
 
-	SHA256_Update(&ctx, _password, _p_length);
+	SHA256_Update(&ctx, _password, passwordLength);
 
-	SHA256_Final(_final_digest, &ctx);
+	SHA256_Final(finalDigest, &ctx);
 
-	return _final_digest;
+	return finalDigest;
 }
 
 void writeHexToFile(unsigned char* buffer, int bufferLength, FILE* file) {
 	for (int i = 0; i < bufferLength; i++) {
 		fprintf(file, "%02X", (unsigned char) buffer[i]);
 	}
-	fputs("\r", file);
+	fputs("\n", file);
 }
 
-unsigned char* fromAsciiToByte(char* input) {
+unsigned char* fromAsciiToByte(unsigned char* input) {
 	unsigned char* ptr, pair[2];
 	long int result;
-	unsigned char * bytes = (unsigned char*)malloc(32);	// hardcoded for SHA-256
-	ptr = (unsigned char*) input;
-	for (unsigned char i = 0; i < 64; i += 2) {
-		memcpy(pair, ptr, 2);
+	// strlen only works if the input is null byte terminated !!!
+	unsigned char* bytes = (unsigned char*)malloc(strlen((char*)input) / 2);
+	ptr = input;
+	for (unsigned char i = 0; i < strlen((char*)input); i += 2) {
+		memcpy(&pair, ptr, 2);
 		result = strtol((char*)pair, NULL, 16);
 		bytes[i / 2] = result;
 		ptr += 2;
@@ -48,7 +54,7 @@ unsigned char* encryptAES(unsigned char* input, unsigned char* IV, AES_KEY key) 
 	// allocate space for ciphertext
 	unsigned char* ciphertext = (unsigned char*)malloc(32);
 	unsigned char _IV[16];
-	memcpy(_IV, IV, 16);
+	memcpy(&_IV, IV, 16);
 
 	AES_cbc_encrypt(input, ciphertext, 32, &key, _IV, AES_ENCRYPT);
 
@@ -72,27 +78,26 @@ int main()
 	if (err == 0) {
 		err2 = fopen_s(&hashesFile, "hashes.txt", "wb");
 		char _char;
-		unsigned char _line_buffer[_BUFFER];
-		int _buffer_index = 0;
+		unsigned char lineBuffer[_BUFFER];
+		int bufferIndex = 0;
 		while ((_char = fgetc(wordlistFile)) != EOF) {
 			// get characters until '\r'	
 			if (_char != '\r') {
-				_line_buffer[_buffer_index++] = _char;
+				lineBuffer[bufferIndex++] = _char;
 			}
 			else {
 				// add '\0' at the end of the line buffer
-				_line_buffer[_buffer_index] = '\0';
+				lineBuffer[bufferIndex] = '\0';
 
 				// compute hash with salt
-				unsigned char* _temp_hash = hashPassword(_line_buffer);
+				unsigned char* _temp_hash = hashPassword(lineBuffer);
 
 				// write password to file
 				writeHexToFile(_temp_hash, SHA256_DIGEST_LENGTH, hashesFile);
-				//fwrite(_line_buffer, 1, SHA256_DIGEST_LENGTH, hashesFile);
 
 				// reset index to get new line
-				_buffer_index = 0;
-				memset(_line_buffer, NULL, _BUFFER);
+				bufferIndex = 0;
+				memset(lineBuffer, NULL, _BUFFER);
 
 				// eat one character to consume the /n
 				fgetc(wordlistFile);
@@ -109,51 +114,101 @@ int main()
 	//		- Each line of the output file enc - sha256.txt represents the hexadecimal format of the encrypted
 	//	SHA - 256 stored on the same line in hashes.txt.
 	err = fopen_s(&hashesFile, "hashes.txt", "rb");
+
 	if (err == 0) {
-		// get IV and key from file
 		FILE* aesConfig, *encFile;
 		err2 = fopen_s(&aesConfig, "aes-cbc.bin", "rb");
 		err3 = fopen_s(&encFile, "enc-sha256.txt", "wb");
+
 		if (err2 == 0 && err3 == 0) {
+			// get IV and key from file
 			unsigned char IV[16], aesKey[256 / 8];
 			fread(IV, 1, 16, aesConfig);
 			fread(aesKey, 1, 256 / 8, aesConfig);
-			// set key
-			AES_KEY aes_key;
 			fclose(aesConfig);
 
+			// set key
+			AES_KEY aes_key;
+			AES_set_encrypt_key(aesKey, 256, &aes_key);
+
 			char _char;
-			char _line_buffer[SHA256_DIGEST_LENGTH + 1];
-			unsigned char *_converted_line;
-			int _buffer_index = 0;
+			unsigned char lineBuffer[SHA256_DIGEST_LENGTH * 2 + 1], * lineBufferInBytes, * encryptedHash;
+			memset(&lineBuffer, 0x00, SHA256_DIGEST_LENGTH * 2 + 1);
+
+			int bufferIndex = 0;
+
 			while ((_char = fgetc(hashesFile)) != EOF) {
 				// get characters until '\r'	
-				if (_char != '\r') {
-					_line_buffer[_buffer_index++] = _char;
+				if (_char != '\n') {
+					lineBuffer[bufferIndex++] = _char;
 				}
 				else {
-
 					// convert from string of hex to unsigned char[]
-					_converted_line = fromAsciiToByte(_line_buffer);
+					lineBufferInBytes = fromAsciiToByte(lineBuffer);
 
 					// encrypt
-					AES_set_encrypt_key(aesKey, 256, &aes_key);
-					unsigned char* _temp_buffer = encryptAES(_converted_line, IV, aes_key);
-					//unsigned char* _temp_buffer = encryptAES(_line_buffer, IV, &aes_key);
+					encryptedHash = encryptAES(lineBufferInBytes, IV, aes_key);
 
 					// write password to file
-					writeHexToFile(_temp_buffer, 32, encFile);
+					writeHexToFile(encryptedHash, 32, encFile);
+					free(encryptedHash);
 
 					// reset index to get new line
-					free(_temp_buffer);
-					free(_converted_line);
-					_buffer_index = 0;
-					memset(_line_buffer, NULL, SHA256_DIGEST_LENGTH + 1);
+					bufferIndex = 0;
+					memset(&lineBuffer, 0x00, SHA256_DIGEST_LENGTH * 2);
 				}
 			}
 			fclose(hashesFile);
 			fclose(encFile);
 		}
 	}
+
+	// 3. Generate the digital signature for the file enc-sha256.txt and save that signature into a file called 
+	// esign.sig.The message digest algorithm is SHA - 256, and the 1024 - bit RSA key for signature
+	// generation is stored in a PEM file named as rsa - key.pem. (5p)
+	FILE* sourceFile = NULL;
+	err = fopen_s(&sourceFile, "enc-sha256.txt", "rb");
+	fseek(sourceFile, 0, SEEK_END);
+	int sourceFileLength = ftell(sourceFile);
+	fseek(sourceFile, 0, SEEK_SET);
+
+	FILE* keyHandler, *destinationFile = NULL;
+	errno_t keyErr = fopen_s(&keyHandler, "rsa-key.pem", "r");
+	// load RSA public key components into RSA structure
+	RSA* pemKey = PEM_read_RSAPrivateKey(keyHandler, NULL, NULL, NULL);
+	fclose(keyHandler);
+
+	err = fopen_s(&destinationFile, "esign.sig", "wb");
+
+	// allocate buffer to store plaintext chunks, eack chunck has 128 bytes == RSA key length
+	unsigned char* sourceBuffer = (unsigned char*)malloc(RSA_size(pemKey) + 1);
+	sourceBuffer[RSA_size(pemKey)] = 0x00;	// null byte terminator
+
+	// allocate buffer to store the ciphertext on 128 bytes same like the RSA key length
+	unsigned char* ciphertextBuffer = NULL;
+	ciphertextBuffer = (unsigned char*)malloc(RSA_size(pemKey));
+
+	if (sourceFileLength != RSA_size(pemKey)) {
+		while (fread_s(sourceBuffer, RSA_size(pemKey), sizeof(unsigned char), RSA_size(pemKey), sourceFile) == RSA_size(pemKey)) {
+			// encryption block-by-block, each block has RSA key length (1024 bits)
+			// because the block is filled in fully, then there is no padding to be used here
+
+			// if the plaintext is matching the number of blocks, the last full block will be encrypted without padding
+			RSA_private_encrypt(RSA_size(pemKey), sourceBuffer, ciphertextBuffer, pemKey, RSA_NO_PADDING);
+			fwrite(ciphertextBuffer, sizeof(unsigned char), RSA_size(pemKey), destinationFile);
+		}
+	}
+	else {
+		fread_s(sourceBuffer, RSA_size(pemKey), sizeof(unsigned char), RSA_size(pemKey), sourceFile);
+	}
+
+	// if there are additional bytes to be encrypted
+	if (sourceFileLength % RSA_size(pemKey))
+	{
+		// encryption of the last block with padding because it could be a partial block (less 1024 bits)
+		RSA_private_encrypt(sourceFileLength % RSA_size(pemKey), sourceBuffer, ciphertextBuffer, pemKey, RSA_PKCS1_PADDING);
+		fwrite(ciphertextBuffer, sizeof(unsigned char), RSA_size(pemKey), destinationFile);
+	}
+
 	return 0;
 }
